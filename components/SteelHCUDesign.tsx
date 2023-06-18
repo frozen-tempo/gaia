b45f395accedddd09350a00e8899a440aaa92688
Luke Gilbert <lukegilbert.416@gmail.com>
2023-06-18 13:57:34 +0100

    adding some features for steel beam supporting HCU

--- components/SteelHCUDesign.tsx
+++ components/SteelHCUDesign.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { InternalSymbolName } from "typescript";
+import { projectData, schemeDesign } from "typings";
+import schemeDesignData from "../src/data/scheme-design-chart-data.json";
+import RayCasting from "./RayCasting";
+import carbonData from "../src/data/carbon-data.json";
+import ProjectSettings from "./ProjectSettings";
 
-function SteelHCUDesign() {
+function SteelHCUDesign(
+  designData: projectData,
+  deadLoadTotal: number,
+  groundDeadTotal: number,
+  roofDeadTotal: number,
+  liveLoadTotal: number,
+  groundLiveTotal: number,
+  roofLiveTotal: number
+) {
+  // Hollowcore Design from Bison / Forterra Precast HCU Load Tables
   return {
     schemeType: "Steel Beam & HCU Slab",
   };
