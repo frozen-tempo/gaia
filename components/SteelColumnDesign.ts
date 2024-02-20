948feb52f46042788176af578dc9cdfee3096d87
Luke Gilbert <lukegilbert.416@gmail.com>
2024-02-20 14:57:56 +0000

    fixed columnDesgin Module

--- components/SteelColumnDesign.ts
+++ components/SteelColumnDesign.ts
@@ -2,20 +2,39 @@ import * as SteelBeamProps from "../src/data/steel-beam-properties.json";
 import { ubBeam, projectData, schemeDesign } from "typings";
 import schemeDesignData from "../src/data/scheme-design-chart-data.json";
 
-function SteelColumnDesign(
-  designData: projectData,
-  internalColumnLoad: number,
-  edgeColumnLoad: number,
-  cornerColumnLoad: number
-) {
+function SteelColumnDesign(designData: projectData, columnLoad: number) {
+  const sectionProps = SteelBeamProps;
   const UCDesignData = schemeDesignData.steelUCColumnDesign;
   const columnHeight = designData.floorHeight;
+  const columnDesignData = Object.entries(UCDesignData);
 
-  Object.keys(UCDesignData).forEach((columnSize) => {
-    console.log(typeof columnHeight);
-  });
+  function getValidColumns(
+    columnData: any,
+    designLoad: number,
+    columnHeight: number
+  ) {
+    var validColumnList: string[] = [];
+    columnData.forEach((column: any) => {
+      const colLengths = Object.keys(column[1]);
+      colLengths.forEach((length) => {
+        const loadCapacity = column[1][length as keyof (typeof column)[1]];
+        if (loadCapacity >= designLoad && parseFloat(length) >= columnHeight) {
+          if (!validColumnList.includes(column[0])) {
+            validColumnList.push(column[0]);
+          }
+        }
+      });
+    });
+    return validColumnList;
+  }
 
-  return;
+  const validColumns = getValidColumns(
+    columnDesignData,
+    columnLoad,
+    columnHeight
+  );
+
+  return validColumns;
 }
 
 export default SteelColumnDesign;
